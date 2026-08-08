f9f427697e27aa4c8b90ddbbbde72ab0e5955dfa
Nelson2SP <nelson2sp@hotmail.com>
2026-08-08 19:41:31 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(cors({
+  origin: '*', // Permite qualquer origem (para testes)
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type']
+}));
 
 // ROTA PRINCIPAL (raiz)
 app.get('/', (req, res) => {
